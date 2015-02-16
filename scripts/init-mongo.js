4d41ff90e5bde040b6c35d88957f459a4fa71b3c
hata <hata@isee.com.tw>
2015-02-16 13:42:23 +0800

    index for user.email

---
+++ scripts/init-mongo.js
@@ -0,0 +1,30 @@
+var indexSpec = {
+    "user" : [
+        {index: {emial:1}, option: {name:"email", unique:true}}
+    ]
+};
+
+["user"].forEach(function(colName){
+    var col = db[colName];
+    if(col.stats().ok != 1) {
+        print("Create collection - " + colName);
+        db.createCollection(colName);
+        col = db[colName];
+    }
+
+    var indexInfo = db[colName].getIndexSpecs();
+    indexSpec[colName].forEach(function(spec){
+        var indexName = spec.option.name;
+        var indexExists = false;
+        indexInfo.forEach(function(index){
+            if(index.name == indexName){
+                indexExists = true;
+                return;
+            }
+        });
+        if(!indexExists) {
+            print("Create index:" + indexName + " for collection:" + colName);
+            col.ensureIndex(spec.index, spec.option);
+        }
+    });
+});
