e241dcab3ec3386ab1e6e6b3b7bb835c970d8109
hata <hata@isee.com.tw>
2015-02-23 16:25:21 +0800

    our own toJson

--- app/models/Model.java
+++ app/models/Model.java
@@ -8,6 +8,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Collection;
 import java.util.Date;
+import java.util.Iterator;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,6 +17,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
 import org.apache.commons.codec.binary.Hex;
+import org.bson.types.ObjectId;
 import org.jongo.Jongo;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
@@ -73,17 +75,32 @@ public class Model
         jedisPool.returnResource(jedis);
     }
 
-    public static ObjectNode toJson(Object object)
+    public static String toJson(Object object)
     {
-        ObjectNode json = mapper.createObjectNode();
+        StringBuilder result = new StringBuilder(512);
 
+        objectToJson(object, result);
+
+        return result.toString();
+    }
+
+    private static void objectToJson(Object object, StringBuilder result)
+    {
         try
         {
-            for (Field field : object.getClass().getDeclaredFields())
+            result.append("{");
+
+            Field[] fields = object.getClass().getDeclaredFields();
+            for (int i = 0; i < fields.length; i++)
             {
+                Field field = fields[i];
+
                 field.setAccessible(true);
                 String name = field.getName();
                 Object value = field.get(object);
+
+                if (name.equals("post_id"))
+                    System.out.println(value.getClass().getName());
 
                 if (field.getAnnotation(JsonProperty.class) != null)
                     name = name.replaceAll("([a-z])([A-Z]+)", "$1_$2").toLowerCase();
@@ -94,69 +111,116 @@ public class Model
                 Class<?> clazz = value.getClass();
                 if (clazz == Boolean.class)
                 {
-                    json.put(name, (Boolean)value);
+                    result.append("\"").append(name).append("\":")
+                        .append((Boolean)value);
                 }
                 else if (clazz == Date.class)
                 {
                     Date date = (Date)value;
-                    json.put(name, date.getTime() / 1000);
+                    result.append("\"").append(name).append("\":")
+                        .append(date.getTime() / 1000);
                 }
                 else if (clazz == Integer.class)
                 {
-                    json.put(name, (Integer)value);
+                    result.append("\"").append(name).append("\":")
+                        .append((Integer)value);
                 }
                 else if (clazz == String.class)
                 {
-                    json.put(name, (String)value);
+                    result.append("\"").append(name).append("\":\"")
+                        .append((String)value).append("\"");
                 }
                 else if (value instanceof Collection)
                 {
-                    Collection collection = (Collection)value;
-                    ArrayNode node = mapper.createArrayNode();
-
-                    if (!collection.isEmpty())
-                    {
-                        clazz = collection.iterator().next().getClass();
-                        if (clazz == Boolean.class)
-                        {
-                            for (Boolean o : (Collection<Boolean>)value)
-                                node.add(o);
-                        }
-                        else if (clazz == Date.class)
-                        {
-                            for (Date o : (Collection<Date>)value)
-                                node.add(o.getTime() / 1000);
-                        }
-                        else if (clazz == Integer.class)
-                        {
-                            for (Integer o : (Collection<Integer>)value)
-                                node.add(o);
-                        }
-                        else if (clazz == String.class)
-                        {
-                            for (String o : (Collection<String>)value)
-                                node.add(o);
-                        }
-                        else
-                        {
-                            for (Object o : (Collection<?>)value)
-                                node.add(toJson(o));
-                        }
-                    }
-                    json.put(name, node);
+                    result.append("\"").append(name).append("\":[");
+                    collectionToJson((Collection)value, result);
+                    result.append("]");
                 }
                 else
                 {
-                    json.put(name, value.toString());
+                    result.append("\"").append(name).append("\":\"")
+                        .append(value).append("\"");
                 }
+
+                if (i + 1 < fields.length)
+                    result.append(",");
             }
+
+            result.append("}");
         }
         catch (Exception e)
         {
             throw new RuntimeException(e);
         }
+    }
 
-        return json;
+    private static void collectionToJson(Collection collection, StringBuilder result)
+    {
+        if (!collection.isEmpty())
+        {
+            Object object = collection.iterator().next();
+            Class clazz = object.getClass();
+            if (clazz == Boolean.class)
+            {
+                Iterator<Boolean> iterator = collection.iterator();
+                while (iterator.hasNext())
+                {
+                    result.append(iterator.next());
+                    if (iterator.hasNext())
+                        result.append(",");
+                }
+            }
+            else if (clazz == Date.class)
+            {
+                Iterator<Date> iterator = collection.iterator();
+                while (iterator.hasNext())
+                {
+                    result.append(iterator.next().getTime() / 1000);
+                    if (iterator.hasNext())
+                        result.append(",");
+                }
+            }
+            else if (clazz == Integer.class)
+            {
+                Iterator<Integer> iterator = collection.iterator();
+                while (iterator.hasNext())
+                {
+                    result.append(iterator.next());
+                    if (iterator.hasNext())
+                        result.append(",");
+                }
+            }
+            else if (clazz == String.class)
+            {
+                Iterator<String> iterator = collection.iterator();
+                while (iterator.hasNext())
+                {
+                    result.append("\"").append(iterator.next()).append("\"");
+                    if (iterator.hasNext())
+                        result.append(",");
+                }
+            }
+            else if (object instanceof Collection)
+            {
+                Iterator<Object> iterator = collection.iterator();
+                while (iterator.hasNext())
+                {
+                    objectToJson(iterator.next(), result);
+                    if (iterator.hasNext())
+                        result.append(",");
+                }
+            }
+            else
+            {
+                Iterator<Object> iterator = collection.iterator();
+                while (iterator.hasNext())
+                {
+                    result.append("\"").append(iterator.next()).append("\"");
+                    if (iterator.hasNext())
+                        result.append(",");
+                }
+            }
+        }
     }
 
     public static String md5(String input)
