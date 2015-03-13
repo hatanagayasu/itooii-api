3eafba436b52a21dae623dc1d2d550782445eb3f
hata <hata@isee.com.tw>
2015-03-13 09:48:18 +0800

    implements Serializable in Model

--- app/models/Model.java
+++ app/models/Model.java
@@ -6,6 +6,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.net.UnknownHostException;
 import java.security.MessageDigest;
@@ -28,7 +29,7 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.exceptions.JedisConnectionException;
 
-public class Model
+public class Model implements Serializable
 {
     public static ObjectMapper mapper = new ObjectMapper();
 
