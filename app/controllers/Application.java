5e67a8c91ca7986e420d9f5b7f6943dbdbf1fd3c
hata <hata@isee.com.tw>
2015-04-16 19:29:56 +0800

    /app/countries, /app/country/:id/cities, /app/languages

---
+++ app/controllers/Application.java
@@ -0,0 +1,53 @@
+package controllers;
+
+import play.Play;
+
+import controllers.annotations.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class Application extends AppController {
+    private static JsonNode countries;
+    private static JsonNode cities;
+    private static JsonNode languages;
+
+    static {
+        init();
+    }
+
+    private static void init() {
+        try {
+            String path = Play.application().path() + "/conf/";
+
+            countries = mapper.readValue(new File(path + "country.json"), JsonNode.class);
+            cities = mapper.readValue(new File(path + "city.json"), JsonNode.class);
+            languages = mapper.readValue(new File(path + "language.json"), JsonNode.class);
+        } catch (IOException e) {
+            errorlog(e);
+        }
+    }
+
+    @Anonymous
+    public static Result getCountry(JsonNode params) {
+        return Ok(countries);
+    }
+
+    @Anonymous
+    @Validation(name = "id", rule = "length=2", require = true)
+    public static Result getCity(JsonNode params) {
+        String id = params.get("id").textValue();
+
+        if (!cities.has(id))
+            return NotFound();
+
+        return Ok(cities.get(id));
+    }
+
+    @Anonymous
+    public static Result getLanguage(JsonNode params) {
+        return Ok(languages);
+    }
+}
