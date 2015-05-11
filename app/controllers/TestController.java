40000f65050a678618917e0731a832b344d86d2e
hata <hata@isee.com.tw>
2015-05-11 13:02:12 +0800

    use webServer

--- app/controllers/TestController.java
+++ app/controllers/TestController.java
@@ -8,7 +8,7 @@ public class TestController extends AppController {
     }
 
     public static play.mvc.Result verifyEmail() {
-        String link = "http://localhost:9999/account/verify-email/" +
+        String link = webServer + "account/verify-email/" +
             "96278552-8abc-41be-a9b8-bfa52c5f13c2";
 
         return ok(views.html.Email.verify_email.render(link));
